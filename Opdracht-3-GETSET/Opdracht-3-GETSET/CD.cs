44c793b4dfea1e2767e7a764887ce47574bb70c4
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:53:29 +0100

    Add files via upload

---
+++ Opdracht-3-GETSET/Opdracht-3-GETSET/CD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_3
+{
+    class CD
+    {
+        private string uitvoerder;
+        private string titel;
+
+        public string Titel
+        {
+            get { return titel; }
+            set { titel = value; }
+        }
+
+        public string Uitvoerder
+        {
+            get { return uitvoerder; }
+            set { uitvoerder = value; }
+        }
+
+        public CD(string Uitvoerder, string Titel)
+        {
+            uitvoerder = Uitvoerder;
+            titel = Titel;
+        }
+        public CD()
+        {
+            titel = "Titel";
+            uitvoerder = "Uitvoerder";
+        }
+        //REMEMBER!!
+        //Optie 2: (check main)
+
+        //public override string ToString()
+        //{
+        //    return this.titel + " - " + this.uitvoerder;
+        //}
+    }
+}
---
+++ Opdracht-3-GETSET/Opdracht-3-GETSET/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Opdracht_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CD> Liedjes = new List<CD>();
+            int keuze;
+            string antwoord;
+
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine("1) Geef nieuw liedje in.");
+                Console.WriteLine("2) Geef CD weer.");
+                Console.WriteLine("3) Exit.");
+                Console.Write("Keuze: ");
+                keuze = Convert.ToInt32(Console.ReadLine());
+
+                if (keuze <= 3)
+                {
+                    switch (keuze)
+                    {
+                        case 1:
+                            {
+                                do
+                                {
+                                    Liedjes.Add((Song()));
+
+                                    Console.Write("Wilt u nog een liedje toevoegen?(J/N): ");
+                                    antwoord = Console.ReadLine();
+                                } while (antwoord.ToUpper() == "J");
+                            }
+                            break;
+
+                        case 2:
+                            {
+                                CD(Liedjes);
+                                Console.ReadLine();
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Dit is geen geldige optie!");
+                    Console.ReadLine();
+                }
+            } while (keuze != 3);
+        }
+
+        private static CD Song()
+        {
+            Console.Clear();
+            CD cd = new CD();
+
+            Console.Write("Geef de titel in: ");
+            cd.Titel = Console.ReadLine();
+            Console.Write("Geef de uitvoerder in: ");
+            cd.Uitvoerder = Console.ReadLine();
+
+            return cd;
+        }
+        private static void CD(List<CD> Liedjes)
+        {
+            if (Liedjes.Count == 0)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                Console.WriteLine("Er zijn nog geen liedjes ingegeven!");
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Liedjes:\n");
+
+                int Teller = 1;
+
+                foreach (CD Cd in Liedjes)
+                {
+                    Console.WriteLine("{0}: {1} - {2}", Teller, Cd.Titel, Cd.Uitvoerder);
+                    Teller++;
+
+                    //REMEMBER!!
+                    //Optie 2: (check class)
+
+                    //Console.WriteLine(Teller + ": " +Cd);
+                    //Teller++;
+                }
+            }
+        }
+    }
+}
